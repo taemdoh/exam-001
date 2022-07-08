396a4c6149f93935ba23fd413cdf1600b2d6e85f
taemdoh <tae.mdoh@gmail.com>
2022-07-09 00:38:05 +0700

    update exam-001

---
+++ pages/test-01/test-01.js
@@ -0,0 +1,38 @@
+/*ข้อ.9
+console.log($("#numberA").val() + " tae");
+*/
+
+/*ข้อ.10
+alert($("#numberB").val() + " tae");
+*/
+
+/*ข้อ.13
+$(".writBackGC").css("background-color", "red");
+*/
+
+/*ข้อ.20
+let A = document.getElementById("numberX").value;
+let B = document.getElementById("numberY").value;
+let sum = A + B
+console.log(sum)
+*/
+
+/*ข้อ.21
+let a = document.getElementById("numberX").value;
+console.log(a)
+if(a == 0) {
+alert("ศูนย์")
+} else if (a > 10) {
+alert("มากกว่า 10")
+} else if (a < 10) {
+alert("น้อยกว่า 10")
+}
+*/
+
+/*ข้อ.22
+function callFnOnSave() {
+document.getElementById(clickForSave)
+alert("callTest")
+}
+*/
+
